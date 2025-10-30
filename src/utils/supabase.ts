78bb8f30af44d58cef0f1056af2378edfba68df2
arffsaad <arffsaad@gmail.com>
2025-10-31 01:13:02 +0800

    Update to use supabase url instead of ip

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,6 +2,12 @@ import { getCookies, setCookie } from '@tanstack/react-start/server'
 import { createServerClient } from '@supabase/ssr'
 
 export function getSupabaseServerClient() {
+  // Define the headers for Cloudflare Access
+  const headers: Record<string, string> = {
+    'CF-Access-Client-Id': '1318c1f29cc77d28797c144b97fc3bdf.access',
+    'CF-Access-Client-Secret': 'aaa0c6140c72bc069b9e7356e67b61ea206d16bc15923c2d8f537cf6bcf32239',
+  }
+
   return createServerClient(
     process.env.SUPABASE_URL!,
     process.env.SUPABASE_ANON_KEY!,
@@ -18,6 +24,10 @@ export function getSupabaseServerClient() {
             setCookie(cookie.name, cookie.value)
           })
         },
+      },
+      // Pass the Cloudflare headers to all requests
+      global: {
+        headers: headers,
       },
     },
   )
